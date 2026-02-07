2aac92fed67ea1f2b31fb4de1b396150beef8502
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2026-02-07 16:20:20 -0500

    modelos, pipes y servicios

---
+++ src/app/pipes/pokemon-image.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Pokemon } from '../models/pokemon.model';
+
+@Pipe({ name: 'pokemonImage' })
+export class PokemonImagePipe implements PipeTransform {
+  transform(pokemon: Pokemon): string {
+    return (
+      pokemon?.sprites?.other?.['official-artwork']?.front_default ||
+      pokemon?.sprites?.front_default ||
+      'assets/pokemon-placeholder.png'
+    );
+  }
+}
